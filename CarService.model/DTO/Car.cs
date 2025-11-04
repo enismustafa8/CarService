eeae0e49baa1078e8ad0bbee7d60331358862f28
enismustafa8 <z1enis792@gmail.com>
2025-11-04 20:08:59 +0200

    Part 1

---
+++ CarService.model/DTO/Car.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CarService.model.DTL
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ CarService.model/StaticDatabase/StaticDB.cs
@@ -0,0 +1,22 @@
+﻿using CarService.model.DTO;
+
+namespace CarService.model.StaticDatabase
+{
+    public static class StaticDB
+    {
+        public static List<Car> Cars { get; set; } = new List<Car>()
+        {
+            new Car() { Id=1, Model="BMW", Year=2010 },
+            new Car() { Id=2, Model="Audi", Year=2012 },
+            new Car() { Id=3, Model="Mercedes", Year=2015 },
+        };
+        public class Car
+        {
+            public int Id { get; set; }
+
+            public string Model { get; set; }
+
+            public int Year { get; set; }
+        }
+    }
+}
